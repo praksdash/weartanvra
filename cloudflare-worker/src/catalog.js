d44688ff6b02b9ea093e6e36c75340ee921bd632
prakash dash <praksdash81@gmail.com>
2026-09-07 14:55:32 +0530

    update pictures

--- cloudflare-worker/src/catalog.js
+++ cloudflare-worker/src/catalog.js
@@ -1,3 +1,3 @@
-export const PRODUCTS={"oversized-rojana-ek-ghanta":749,"oversized-edge-01":599,"oversized-core-220":549,"oversized-ghost-compass":699,"oversized-not-fast-just-furious":649,"oversized-unleash-the-beast":649,"oversized-wild-instinct":649};
+export const PRODUCTS={"oversized-rojana-ek-ghanta":749,"oversized-edge-01":549,"oversized-core-220":549,"oversized-ghost-compass":699,"oversized-not-fast-just-furious":649,"oversized-unleash-the-beast":649,"oversized-wild-instinct":649};
 export const PREPAID_COUPON_CODE="PREPAID50";
 export const PREPAID_DISCOUNT=50;
